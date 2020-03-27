187bc0b423ede926978dba7ad2fed07883f51ae3
Katarina <katarina@udel.edu>
2020-03-26 23:44:59 -0400

    buttons and drag work

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -16,6 +16,11 @@ export default class MainScene extends Phaser.Scene {
   private pHlabel: any;
   private pH: any;
   private compoundLabel: any;
+  private beakerimage: any;
+  private otherbox: any;
+  private filledbeaker: any;
+  private pHmeter: any;
+  private notUpdated: boolean;
 
   constructor() {
     super({ key: 'MainScene' });
@@ -23,11 +28,26 @@ export default class MainScene extends Phaser.Scene {
 
 
   create() {
+    this.notUpdated=true;
     this.add.text(5, 10, "Experiment 20: pH and its Applications", {fill: "#000000", fontSize: '16px'});
     this.add.text(5, 40, "Select a \ncompound:", {fill: "#0000ff", fontSize: "12px"});
     this.box=this.add.image(75,360,"bluebackground");
     this.box.rotation+=0.00;
     this.box.setScale(0.1);
+
+    this.otherbox=this.physics.add.image(300, 360, "bluebackground");
+    this.otherbox.setScale(0.05);
+    this.otherbox.setAlpha(0.0);
+
+    this.beakerimage=this.add.image(300, 300, "emptybeaker");
+    this.beakerimage.setScale(0.4);
+    this.beakerimage.setAlpha(1);
+
+    this.filledbeaker=this.add.image(300, 300, "filledbeaker");
+    this.filledbeaker.setScale(0.4);
+    this.filledbeaker.setAlpha(0.0);
+    
+    
 
     this.unknown1button= this.add.text(5, 75, "20-39X", {fill: "#0f0"})
      .setInteractive()
@@ -81,7 +101,16 @@ export default class MainScene extends Phaser.Scene {
     this.compoundLabel=this.add.bitmapText(5, 300, "pixelFont");
     this.compoundLabel.fontSize=20;
     this.compoundLabel.text=this.compound + " has \nbeen selected";
-   
+
+    this.pHmeter=this.physics.add.image(300, 100, "pHmeter");
+    this.pHmeter.setScale(0.8);
+    this.pHmeter.setInteractive();
+    this.input.setDraggable(this.pHmeter);
+
+    this.input.on('drag', this.doDrag, this)
+
+    this.physics.add.overlap(this.otherbox, this.pHmeter, this.updatepH, undefined, this);
+
     //this.player=this.physics.add.image(this.scale.width/2, this.scale.height-64, "dog");
     //this.player.setScale(.1);
     //this.cursorKeys=this.input.keyboard.createCursorKeys();
@@ -101,18 +130,29 @@ export default class MainScene extends Phaser.Scene {
   displaySelected(){
     if (this.compound !== "N/A"){
       this.compoundLabel.setText(this.compound + " has \nbeen selected");
+      this.beakerimage.setAlpha(0.0);
+      this.filledbeaker.setAlpha(1.0);
       //this.add.text(5, 280, this.compound + "has been selected", {fill: "#000000", fontSize: "10"});
     }
   }
 
+  doDrag(pointer){
+    this.pHmeter.x=pointer.x;
+    this.pHmeter.y=pointer.y;
+  }
+
   updateCompound(name){
     this.compound=name;
-    this.updatepH();
   }
 
   updatepH(){
-    if (this.compound==="unknown 1"){
-      this.pH="1.92";
+    var x = (Math.random()-0.5)/Math.pow(10,1);
+    if (this.compound==="unknown 1"&&this.notUpdated){
+      this.pH=(1.92+x).toString().substring(0,4);
+      this.notUpdated=false;
+    }
+    if (this.compound==="unknown 2"){
+      this.pH="4.1";
     }
     if (this.compound==="null"){
       this.pH="-.--";
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -8,6 +8,9 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('unknown1button', "assets/images/background.png");
     this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml");
     this.load.bitmapFont("font1", "assets/font/font1.fnt");
+    this.load.image("emptybeaker", "assets/images/emptybeaker.png");
+    this.load.image("filledbeaker", "assets/images/filledbeaker.png");
+    this.load.image("pHmeter", "assets/images/pHmeter.png");
   }
 
   create() {
